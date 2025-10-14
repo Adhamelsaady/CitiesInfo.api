9d3b00573ab51886c4e1bb44ad46b302f2273719
adhamelsaady <adhamelsaady81@gmail.com>
2025-10-14 08:33:04 +0300

    Project added

---
+++ CitiesApi/Controllers/CitiesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace CitiesApi.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet("api/Cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object> {
+
+                new {id = 1 , Name = "Cairo"} , 
+                new {id = 2 , Name = "Alexandria "}
+            });
+        }
+    }
+}
